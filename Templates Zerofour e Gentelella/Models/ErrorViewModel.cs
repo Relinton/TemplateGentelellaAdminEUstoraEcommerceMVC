28ead3aa4086e1401dd96ad45844b3a22608aa22
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2022-11-03 17:53:47 -0400

    Projeto mvc Aplicado Template Ecommerce Ustora e Template Admin Gentelella com Bootstrap

---
+++ Templates Zerofour e Gentelella/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Templates_Zerofour_e_Gentelella.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
